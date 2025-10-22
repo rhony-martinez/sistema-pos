3e64281f8ef3ba4057a143f579d9f82854bd55c3
rhony-martinez <rhony@unicauca.edu.co>
2025-10-22 00:53:40 -0500

    Finalización de flujo normal

--- SistemaPOS/SistemaPOS.Web/wwwroot/js/login.js
+++ SistemaPOS/SistemaPOS.Web/wwwroot/js/login.js
@@ -44,7 +44,7 @@
                     window.location.href = "dashboard-adm-local.html";
                     break;
                 case "CAJERO":
-                    window.location.href = "dashboard-cajero.html";
+                    window.location.href = "gestion-caja.html";
                     break;
                 default:
                     alert("Rol no reconocido.");
